a0239a554e0c5d8c08f7be3d58f591b07ec3bda8
In2013 <3159871+In2013@users.noreply.github.com>
2021-04-11 21:56:48 +0800

    Create one.sh

---
+++ one.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /root/ &&
+curl -sL https://github.com/eysp/public/archive/public.tar.gz | tar xz &&
+rm -rf public &&
+mv public-public public &&
+docker stop portainer &&
+docker rm portainer &&
+docker rmi portainer/portainer:linux-arm64 &&
+docker rmi portainer/portainer-ce:linux-arm64 &&
+docker run -d -p 8000:8000 -p 9000:9000 --name="portainer" --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data -v /root/public:/public portainer/portainer-ce:linux-arm64
